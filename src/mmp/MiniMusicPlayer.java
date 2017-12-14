5851d257f300f6793302302d427adc1868628b5d
lost273 <dnk273@gmail.com>
2017-12-14 22:34:05 +0300

    14-12-2017

--- src/mmp/MiniMusicPlayer.java
+++ src/mmp/MiniMusicPlayer.java
@@ -42,11 +42,7 @@ public class MiniMusicPlayer {
             ex.printStackTrace();
         }
     }
-
-    public void controlChange(ShortMessage event){
-        System.out.println("ля");
-    }
-    public static MidiEvent makeEvent(int comd, int chan, int one, int two, int tick){
+    public MidiEvent makeEvent(int comd, int chan, int one, int two, int tick){
         MidiEvent event = null;
         try {
             ShortMessage a = new ShortMessage();
@@ -54,5 +50,32 @@ public class MiniMusicPlayer {
             event = new MidiEvent(a, tick);
         } catch (Exception e){}
         return event;
+    }
+    class MyDrawPanel extends JPanel implements ControllerEventListener{
+        boolean msg = false;
+        public void controlChange(ShortMessage event){
+            msg = true;
+            repaint();
+        }
+        public void paintComponent(Graphics g){
+            if (msg){
+                Graphics2D g2 = (Graphics2D) g;
+                
+                int r = (int)(Math.random() * 250);
+                int gr = (int)(Math.random() * 250);
+                int b = (int)(Math.random() * 250);
+                
+                g.setColor(new Color(r, gr, b));
+                
+                int ht = (int)((Math.random() * 120) + 10);
+                int width = (int)((Math.random() * 120) + 10);
+                
+                int x = (int)((Math.random() * 40) + 10);
+                int y = (int)((Math.random() * 40) + 10);
+                
+                g.fillRect(x, y, ht, width);
+                msg = false;
+            }
+        }
     }
 }
